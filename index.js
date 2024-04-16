570d64e4afc5e0a1107577f84c4e161da49e510f
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2024-04-16 19:11:10 +0000

    adding switch

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const core = require('@actions/core');
 
 try {
   const nameToGreet = core.getInput('name');
+  const isOn = core.getBooleanInput('sw')
   console.log(`Hello ${nameToGreet}!`);
+  if (sw) {
+    console.log("switch is on");
+  } else {
+    console.log("switch is off");
+  }
   core.setOutput("greeting", `Hello ${nameToGreet}!`);
 } catch (error) {
   core.setFailed(error.message);
