df7327ff1c4274740fd79a3ad2e242a7a312e7f3
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2024-04-16 19:39:22 +0000

    adding code

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const core = require('@actions/core');
 const axios = require('axios');
 
 
-try {
-  const response = await axios.get(apiUrl);
-  core.setOutput('Response:', response.data);
+async function makeGetRequest(url) {
+  try {
+      const response = await axios.get(apiUrl);
+      core.setOutput('Response:', response.data);
   } catch (error) {
-  core.setFailed('Error:', error);
+      core.setFailed('Error:', error);
+  }
 }
+
+// Call the function to make the GET request
+const url = core.getInput('url');
+makeGetRequest(url);
